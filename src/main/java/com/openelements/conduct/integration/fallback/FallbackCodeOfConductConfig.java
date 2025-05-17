5e3b3b0362db1317672e16379a01c3655af26183
Daniel Ntege <danientege785@gmail.com>
2025-05-17 14:13:39 +0300

    Refactor configuration classes: remove unused imports, streamline property handling, and enhance logging for fallback conduct checker
    Signed-off-by: Daniel Ntege <danientege785@gmail.com>

--- src/main/java/com/openelements/conduct/integration/fallback/FallbackCodeOfConductConfig.java
+++ src/main/java/com/openelements/conduct/integration/fallback/FallbackCodeOfConductConfig.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 
 @Configuration
 public class FallbackCodeOfConductConfig {
--- src/main/java/com/openelements/conduct/integration/file/FileConfig.java
+++ src/main/java/com/openelements/conduct/integration/file/FileConfig.java
@@ -4,7 +4,6 @@ import com.openelements.conduct.data.CodeOfConductProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ConditionalOnProperty(
@@ -15,7 +14,6 @@ import org.springframework.context.annotation.Primary;
 public class FileConfig {
 
     @Bean
-    @Primary
     CodeOfConductProvider fileBasedCodeOfConductProvider() {
         return new FileBasedCodeOfConductProvider();
     }
